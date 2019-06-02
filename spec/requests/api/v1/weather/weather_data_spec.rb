0956c7281d40f762c8105a71800488bba0eadc06
Matt Weiss <weiss.matt@outlook.com>
2019-06-01 22:50:50 -0600

    Adds params to weather data request spec

--- spec/requests/api/v1/weather/weather_data_spec.rb
+++ spec/requests/api/v1/weather/weather_data_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'weather API' do
   it 'sends weather for a location' do
-    get '/api/v1/forecast'
 
+    get '/api/v1/forecast?location=denver,co'
     expect(response).to be_successful
   end
 end
