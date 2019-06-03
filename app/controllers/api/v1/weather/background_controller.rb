91b29fcb21e46f0b97fe4983882fe621324dd4c6
Matt Weiss <weiss.matt@outlook.com>
2019-06-02 19:11:01 -0600

    Adds background controller

---
+++ app/controllers/api/v1/weather/background_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::Weather::BackgroundController < ApplicationController
+
+  def show
+    location = Location.find_or_create_by(city_state: search_params[:location])
+    if location.background_url
+      render json: BackgroundSerializer.new(location)
+    else
+      background = get_background(search_params[:location])
+      render json: BackgroundSerializer.new(background)
+    end
+  end
+
+  private
+
+  def get_background(location)
+    location_info = LocationService.new(location).parse_location
+    background = BackgroundService.new(location_info)
+    background.update_backgrounds
+  end
+
+  def search_params
+    params.permit(:location)
+  end
+end
