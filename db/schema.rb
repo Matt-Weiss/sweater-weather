4184ac0162212f7d2622312e646c371c22af8a89
Matt Weiss <weiss.matt@outlook.com>
2019-06-03 11:18:38 -0600

    Adds formatted_address column to table for a more user friendly experience

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_221050) do
+ActiveRecord::Schema.define(version: 2019_06_03_170933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_221050) do
     t.string "longitude"
     t.json "forecast"
     t.json "background_url"
+    t.string "formatted_address"
   end
 
 end
