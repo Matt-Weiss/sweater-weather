969fcf1d0b01ccd608c391a16486e95c43434d2f
Matt Weiss <weiss.matt@outlook.com>
2019-06-03 10:21:14 -0600

    Completes methods to find antipode coordinates

--- app/services/antipode_service.rb
+++ app/services/antipode_service.rb
@@ -5,16 +5,17 @@ class AntipodeService
   end
 
   def find_antipode
-    data = get_json
-    # binding.pry
+    lat = get_json[:data][:attributes][:lat]
+    long = get_json[:data][:attributes][:long]
+    antipode_location = LocationService.new({lat: lat, long: long})
+    antipode_location.antipode_data
   end
-
-  private
 
   def get_json
-    response = conn.get
+    response ||= conn.get
     JSON.parse(response.body, symbolize_names: true)
   end
+  private
 
   def conn
     Faraday.new("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{@location.latitude}&long=#{@location.longitude}") do |f|
