df4e033567b76187f1025c2708c19a92b865289a
Matt Weiss <weiss.matt@outlook.com>
2019-06-02 19:10:09 -0600

    Adds methods and POROS for fetching and displaying forecast data.

--- app/controllers/api/v1/weather/forecast_controller.rb
+++ app/controllers/api/v1/weather/forecast_controller.rb
@@ -1,16 +1,22 @@
 class Api::V1::Weather::ForecastController < ApplicationController
 
   def show
-    weather = Location.find_by(city_state: search_params[:location])
-    if weather
-      render json: weather.to_json
+    location = Location.find_or_create_by(city_state: search_params[:location])
+    if location.forecast
+      render json: WeatherSerializer.new(location)
     else
-      Forecast.new(search_params[:location])
+      location = get_weather(search_params[:location])
+      render json: WeatherSerializer.new(location)
     end
   end
 
-
   private
+
+  def get_weather(location)
+    location_info = LocationService.new(location).parse_location
+    weather = ForecastService.new(location_info)
+    weather.get_forecast
+  end
 
   def search_params
     params.permit(:location)
