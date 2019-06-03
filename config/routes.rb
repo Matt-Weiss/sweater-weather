6e3c19c09efccf779b885bfc9b86c373cf9eed90
Matt Weiss <weiss.matt@outlook.com>
2019-06-02 19:09:33 -0600

    Adds routes for forecast and background

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-        get '/forecast', to: 'weather/forecast#show'
+      get '/forecast', to: 'weather/forecast#show'
+      get '/backgrounds', to: 'weather/background#show'
     end
   end
 end
