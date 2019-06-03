c870feca96da3931df09a86dcf6d0ae8bc094814
Matt Weiss <weiss.matt@outlook.com>
2019-06-03 10:54:36 -0600

    Builds out serialization for desired output

---
+++ app/serializers/antipode_serializer.rb
@@ -0,0 +1,17 @@
+class AntipodeSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attribute :location_name do |object|
+    object.city_state
+  end
+
+  attribute :forecast do |object|
+    object.forecast = {summary: object.forecast["currently"]["summary"],
+           current_temperature: object.forecast["currently"]["temperature"]
+                      }
+  end
+  
+  attribute :search_location do |object, params|
+    params[:original_location].city_state
+  end
+end
