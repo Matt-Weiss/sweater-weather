fef897c69e824fd2fa0e5c4799de42ca125829b8
Matt Weiss <weiss.matt@outlook.com>
2019-06-02 19:09:02 -0600

    Adds test for forecast endpoint

--- spec/requests/api/v1/weather/weather_data_spec.rb
+++ spec/requests/api/v1/weather/weather_data_spec.rb
@@ -5,5 +5,9 @@ describe 'weather API' do
 
     get '/api/v1/forecast?location=denver,co'
     expect(response).to be_successful
+    #test that location is now in db by hitting endpoint again
+    #the second hit should establish coverage on this path in controller
+    get '/api/v1/forecast?location=denver,co'
+    expect(response).to be_successful
   end
 end
