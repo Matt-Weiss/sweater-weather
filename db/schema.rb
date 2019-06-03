4832ee0721c7878c4310cac21fb7de5c5ebd8555
Matt Weiss <weiss.matt@outlook.com>
2019-06-02 19:08:16 -0600

    Adds database with table for storing weather data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_035633) do
+ActiveRecord::Schema.define(version: 2019_06_02_221050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_035633) do
     t.string "latitude"
     t.string "longitude"
     t.json "forecast"
+    t.json "background_url"
   end
 
 end
