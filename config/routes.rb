6b260cbce36fee7639c9814218640782113091ba
Matt Weiss <weiss.matt@outlook.com>
2019-06-03 20:22:41 -0600

    Adds route for new user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'weather/forecast#show'
       get '/backgrounds', to: 'weather/background#show'
+      post '/users', to: 'user#create'
     end
   end
 end
