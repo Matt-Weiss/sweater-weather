335c1b822ac454be66509101b411201496948970
Matt Weiss <weiss.matt@outlook.com>
2019-06-04 13:40:48 -0600

    Moves user/api_key validation to BaseAPIController

--- app/controllers/api/v1/favorite_controller.rb
+++ app/controllers/api/v1/favorite_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::FavoriteController < BaseAPIController
 
   def create
-    user = User.find(session[:user_id])
-    if user.api_key == params[:api_key]
-      favorite = Favorite.create(user_id: user.id,
+    if valid_user?
+      favorite = Favorite.create(user_id: session[:user_id],
                              location_id: params[:location])
       render json: {success: "favorite stored!"}, status: 201
     else
