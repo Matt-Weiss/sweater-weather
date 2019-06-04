973b8db237c9c316df3b10c07c3723fec88b9a98
Matt Weiss <weiss.matt@outlook.com>
2019-06-04 16:19:01 -0600

    Adds delete favorites route

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
       get '/forecast', to: 'weather/forecast#show'
       get '/backgrounds', to: 'weather/background#show'
       get '/favorites', to: 'favorite#index'
-      
+
       post '/users', to: 'user#create'
       post '/sessions', to: 'session#create'
       post '/favorites', to: 'favorite#create'
 
+      delete '/favorites', to: 'favorite#delete'
     end
   end
 end
