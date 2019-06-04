2719274157a9ebf4aa0c38f1b6a31b3ae18e2f68
Matt Weiss <weiss.matt@outlook.com>
2019-06-04 09:27:16 -0600

    Adds relationship tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :request do
+
   it 'can create a user and return an api key' do
 
     post "/api/v1/users", :params => {email: "email@test.com",
@@ -22,5 +23,11 @@ RSpec.describe User, type: :request do
     json = JSON.parse(response.body, symbolize_names: true)
     expect(response.status).to eq(200)
     expect(json[:api_key]).to_not eq(nil)
+  end
+end
+
+RSpec.describe User, type: :model do
+  describe 'relationships' do
+    it { should have_many(:locations)}
   end
 end
