90025faaed30662724b45b49fe73d33cbc4e5705
Matt Weiss <weiss.matt@outlook.com>
2019-06-04 23:18:06 -0600

    made fetch happen. broke some things into smaller pieces for SRP.

--- app/controllers/api/v1/weather/forecast_controller.rb
+++ app/controllers/api/v1/weather/forecast_controller.rb
@@ -2,19 +2,18 @@ class Api::V1::Weather::ForecastController < ApplicationController
 
   def show
     location = Location.find_or_create_by(city_state: search_params[:location])
-    if location.forecast
-      render json: WeatherSerializer.new(location)
-    else
+    forecast = Rails.cache.fetch("location-#{location.id}-forecast", expires_in: 15.minutes) do
       get_weather(location)
-      render json: WeatherSerializer.new(location)
     end
+
+    render json: WeatherSerializer.full_forecast(forecast)
   end
 
   private
 
   def get_weather(location)
     location = LocationService.new(location).parse_location
-    weather = ForecastService.new(location)
+    weather = LocationForecastRetriever.new(location)
     weather.get_forecast
   end
 
